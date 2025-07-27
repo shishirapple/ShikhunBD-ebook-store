cfd0ce48d757df82995183d592db11a31798a9df
shishirapple <shishirappleid1@gmail.com>
2025-07-27 08:10:08 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { pushToDataLayer } from './dataLayer';
+
+function App() {
+  useEffect(() => {
+    pushToDataLayer({
+      event: 'page_view',
+      pageCategory: 'home',
+      pageTitle: document.title,
+      userType: 'guest', // replace with real user data if available
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to ShikhunBD Ebook Store</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// Mounting App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
